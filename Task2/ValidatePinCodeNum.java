0757cfe2e1069fee075653f3a07055dd2b5c400d
Roushan Kumar <roushank559@gmail.com>
2023-03-15 21:21:34 +0530

    commit

--- Task2/ValidatePinCodeNum.java
+++ Task2/ValidatePinCodeNum.java
@@ -1,11 +1,26 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class ValidatePinCodeNum {
     public static void main(String[] args) {
-        int num=0;
-        
-        System.out.println(num+ " " +isValidPinNumber());
+        String num=null;
+        Scanner sc = new Scanner(System.in);
+        num = sc.nextLine();
+        sc.close();
+        System.out.println(num+ " " +isValidPinNumber(num));
     }
 
-    private static boolean isValidPinNumber() {
-        return false;
+    private static boolean isValidPinNumber(String pinCode) {
+        String regex = "^[1-9]{1}[0-9]{2}\\s{0,1}[0-9]{3}$";
+	        
+	        Pattern p = Pattern.compile(regex);
+	        
+	        if (pinCode == null) {
+	            return false;
+	        }
+	        Matcher m = p.matcher(pinCode);
+
+	        return m.matches();
     }
 }
